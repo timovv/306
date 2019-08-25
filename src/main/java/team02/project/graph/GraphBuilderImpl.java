44b071353c16a34e0986a1977f78c4142a1eb63f
TwelveHertz <twelvehertz@gmail.com>
2019-08-25 18:17:05 +1200

    added docs for io and graph packages

--- src/main/java/team02/project/graph/GraphBuilderImpl.java
+++ src/main/java/team02/project/graph/GraphBuilderImpl.java
@@ -16,6 +16,11 @@ public class GraphBuilderImpl implements GraphBuilder {
 
     private int currentIndex = 0;
 
+    /**
+     * Builds graph by setting up all nodes, dependencies, top and bottom level
+     *
+     * @return the graph that has been built
+     */
     @Override
     public Graph build() {
         built = true;
@@ -23,8 +28,8 @@ public class GraphBuilderImpl implements GraphBuilder {
         LinkedHashSet<MutableNode> order = new LinkedHashSet<>();
         LinkedHashSet<MutableNode> reverseOrder = new LinkedHashSet<>();
 
-        for(MutableNode node : wip.getNodes()) {
-            topologicalVisit(order,  node);
+        for (MutableNode node : wip.getNodes()) {
+            topologicalVisit(order, node);
             reverseTopologicalVisit(reverseOrder, node);
         }
 
@@ -36,11 +41,11 @@ public class GraphBuilderImpl implements GraphBuilder {
     }
 
     /**
-     * Visits each Node, records the topological order and set their depedents
+     * Visits each Node, records the topological order and set their dependencies
      *
-     * @param order
-     * @param toVisit
-     * @return
+     * @param order   topological order as a {@link LinkedHashSet}
+     * @param toVisit the node {@link MutableNode} to visit and set up dependencies
+     * @return a {@link Set<Node>} of dependencies
      */
     private Set<Node> topologicalVisit(LinkedHashSet<MutableNode> order, MutableNode toVisit) {
         Set<Node> dependencies = new HashSet<>();
@@ -59,15 +64,22 @@ public class GraphBuilderImpl implements GraphBuilder {
         return dependencies;
     }
 
+    /**
+     * Visits each Node, records the reverse topological order and set their dependents
+     *
+     * @param reverseOrder reverse topological order as a {@link LinkedHashSet}
+     * @param toVisit      the node {@link MutableNode} to visit and set up dependents
+     * @return a {@link Set<Node>} of dependents
+     */
     private Set<Node> reverseTopologicalVisit(LinkedHashSet<MutableNode> reverseOrder, MutableNode toVisit) {
-        if(reverseOrder.contains(toVisit)) {
+        if (reverseOrder.contains(toVisit)) {
             return toVisit.getDependents();
         }
 
         Set<Node> dependents = new HashSet<>();
 
-        for(Node node : toVisit.getOutgoingEdges().keySet()) {
-            dependents.addAll(reverseTopologicalVisit(reverseOrder, (MutableNode)node));
+        for (Node node : toVisit.getOutgoingEdges().keySet()) {
+            dependents.addAll(reverseTopologicalVisit(reverseOrder, (MutableNode) node));
             dependents.add(node);
         }
 
@@ -76,13 +88,16 @@ public class GraphBuilderImpl implements GraphBuilder {
         return dependents;
     }
 
+    /**
+     * Sets the path from each node to the bottom of the graph
+     */
     private void calculateBottomLevels() {
         // do the BFS to get bottom levels of wip
         Queue<MutableNode> queue = wip.getNodes().stream()
                 .filter(x -> x.getOutgoingEdges().isEmpty())
                 .collect(Collectors.toCollection(LinkedList::new));
 
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             val node = queue.poll();
             val bottomLevel = node.getOutgoingEdges().keySet()
                     .stream()
@@ -91,18 +106,21 @@ public class GraphBuilderImpl implements GraphBuilder {
                     .orElse(0) + node.getWeight();
             node.setBottomLevel(bottomLevel);
 
-            for(val node2 : node.getIncomingEdges().keySet()) {
-                queue.offer((MutableNode)node2);
+            for (val node2 : node.getIncomingEdges().keySet()) {
+                queue.offer((MutableNode) node2);
             }
         }
     }
 
+    /**
+     * Sets the path from the top of the graph to each node
+     */
     private void calculateTopLevels() {
         Queue<MutableNode> queue = wip.getNodes().stream()
                 .filter(x -> x.getIncomingEdges().isEmpty())
                 .collect(Collectors.toCollection(LinkedList::new));
 
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             val node = queue.poll();
             val topLevel = node.getIncomingEdges().keySet()
                     .stream()
@@ -111,37 +129,55 @@ public class GraphBuilderImpl implements GraphBuilder {
                     .orElse(0) + node.getWeight();
             node.setTopLevel(topLevel);
 
-            for(val node2 : node.getOutgoingEdges().keySet()) {
-                queue.offer((MutableNode)node2);
+            for (val node2 : node.getOutgoingEdges().keySet()) {
+                queue.offer((MutableNode) node2);
             }
         }
     }
 
     private void throwIfBuilt() {
-        if(built) {
+        if (built) {
             throw new RuntimeException("Can't update; graph has been already built!");
         }
     }
 
+    /**
+     * Sets the ID of graph
+     *
+     * @param id The id as {@link String}
+     */
     @Override
     public void setId(String id) {
         throwIfBuilt();
         wip.setId(id);
     }
 
+    /**
+     * Adds a node to the graph
+     *
+     * @param id     The id of node as {@link String}
+     * @param weight The duration of task represented by node
+     */
     @Override
     public void addNode(String id, int weight) {
         throwIfBuilt();
         wip.addNode(new MutableNode(id, weight, currentIndex++));
     }
 
+    /**
+     * Adds an edge between two nodes, sets up incoming edge to child and outgoing edge from parent
+     *
+     * @param fromId The parent {@link Node}
+     * @param toId   The child {@link Node}
+     * @param weight The delay caused by communication
+     */
     @Override
     public void addEdge(String fromId, String toId, int weight) {
         throwIfBuilt();
         val head = wip.getNode(toId);
         val tail = wip.getNode(fromId);
 
-        if(head == null || tail == null) {
+        if (head == null || tail == null) {
             throw new RuntimeException("Node(s) do not exist for edge");
         }
 
